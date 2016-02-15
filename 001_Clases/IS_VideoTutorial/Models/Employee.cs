888a48400f80d6f676c11e7628cb25b05c608cd5
Ricard Burriel <ricard.burriel@gmail.com>
2016-02-15 09:47:56 +0100

    001 Classes

---
+++ 001_Clases/IS_VideoTutorial/Models/Employee.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Practices.Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS_VideoTutorial.Models
+{
+    public class Employee : BindableBase
+    {
+        private string _name;
+
+        public string Name
+        {
+            get { return _name; }
+            set { SetProperty(ref _name, value); }
+        }
+    }
+}
---
+++ 001_Clases/IS_VideoTutorial/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,56 @@
+﻿using IS_VideoTutorial.Models;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Mvvm;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IS_VideoTutorial.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        #region Fields
+        private string _employeeName;
+        private ObservableCollection<Employee> _employeeCollection;
+
+        public string EmployeeName
+        {
+            get { return _employeeName; }
+            set { SetProperty(ref _employeeName, value); }
+        }
+
+        public ObservableCollection<Employee> EmployeeCollection
+        {
+            get { return _employeeCollection; }
+            set { SetProperty(ref _employeeCollection, value); }
+        }
+
+
+        #endregion
+
+        #region Commands
+        public DelegateCommand OKButtonCommand { get; private set; }
+
+        public void OKButton()
+        {
+            if (!String.IsNullOrWhiteSpace(EmployeeName))
+            {
+                Employee employee = new Employee();
+                employee.Name = EmployeeName;
+                
+                EmployeeCollection.Add(employee);
+                EmployeeName = "";
+            }
+        }
+        #endregion
+
+        public MainWindowViewModel()
+        {
+            OKButtonCommand = new DelegateCommand(OKButton);
+            EmployeeCollection = new ObservableCollection<Employee>();
+        }
+    }
+}
